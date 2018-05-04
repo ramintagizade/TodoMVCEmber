aca110704c16cd94a638ef6e4c411a31d77440fa
Ramin Taghizada <ramintagizade@mail.ru>
2018-05-04 13:09:15 +0400

    remove , check todo

--- app/components/todos-list.js
+++ app/components/todos-list.js
@@ -1,20 +1,40 @@
 import Component from '@ember/component'
 
 export default Component.extend({
-  todos: JSON.parse(localStorage.getItem('todos')) || [],
 
   init () {
     this._super(...arguments)
+    this.set('todos', this.getTodos())
+  },
+  getTodos () {
+  	 return JSON.parse(localStorage.getItem('todos')) || []
+  },
+  setTodos (todos) {
+  	this.set('todos', todos)
+  	localStorage.setItem('todos', JSON.stringify(todos))
   },
   actions: {
 	  	addTodo (name) {
 	  		if (!name) { return }
 
 		    this.get('todos').pushObject(
-		     	name
+		     	{name: name, checked: false}
 		    )
 		    localStorage.setItem('todos', JSON.stringify(this.get('todos')))
 		    this.set('todo-list', '')
+	  	},
+	  	checkTodo (id) {
+	  		let todos = this.getTodos().map(x => {
+	  			if (x.name == id) { x.checked = !x.checked }
+	  			return x
+	  		})
+	  		this.setTodos(todos)
+	  	},
+	  	removeTodo (id) {
+	  		let todos = this.getTodos().filter(x => {
+	  			return x.name != id
+	  		})
+	  		this.setTodos(todos)
 	  	}
   },
   getData: function () {
